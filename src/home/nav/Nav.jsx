9b2408d12ff4ce11b42f4c7663d0fd5c34c47980
Oriya Shrestha <oriya.shrestha111@gmail.com>
2022-03-18 09:21:18 +0545

    variants added

--- src/home/nav/Nav.jsx
+++ src/home/nav/Nav.jsx
@@ -4,22 +4,42 @@ import { FcBusinesswoman } from "react-icons/fc";
 import { motion } from "framer-motion";
 import "./nav.css";
 
+const navVariants = {
+	hidden: {
+		opacity: 0,
+	},
+	visible: {
+		opacity: 1,
+		transition: {
+			delay: 0.19,
+			duration: 3,
+		},
+	},
+};
+
+// hidden vaneko initial ma visible vaneko animate, animate mai transition embed gareyxi no need to write again
+
+const ulVariants = {
+	hidden: {
+		y: -350,
+	},
+	visible: {
+		y: 0,
+		transition: {
+			delay: 0.2,
+			duration: 3,
+			type: "spring",
+			stiffness: 150,
+		},
+	},
+};
+
 const Nav = () => {
 	return (
 		<div>
-			<motion.nav
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ delay: 0.19, duration: 3 }}>
-				<motion.ul
-					initial={{ y: -350 }}
-					animate={{ y: 0 }}
-					transition={{
-						delay: 0.2,
-						duration: 3,
-						type: "spring",
-						stiffness: 150,
-					}}>
+			{/* variants lekhe paxi initial ra hidden parent div lai diye paxi child ley tyei linxa.  */}
+			<motion.nav variants={navVariants} initial='hidden' animate='visible'>
+				<motion.ul variants={ulVariants}>
 					<motion.li whileHover={{ scale: 1.1, color: "#000" }}>
 						<Link to='/'> Home</Link>
 					</motion.li>
